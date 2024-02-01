0e833f4e8c42b369087547b273294c328d3fc928
長岡聡太 <>
2024-02-01 23:18:11 +0900

    cliffy

--- main.ts
+++ main.ts
@@ -1,32 +1,15 @@
-import { Command } from "https://deno.land/x/cliffy@v1.0.0-rc.3/command/mod.ts";
-import { serve } from "https://deno.land/std@0.196.0/http/server.ts";
+import { colors, tty } from "https://deno.land/x/cliffy@v1.0.0-rc.3/ansi/mod.ts";
+import { delay } from "https://deno.land/std@0.196.0/async/delay.ts";
 
+const error = colors.bold.red;
+const warn = colors.bold.yellow;
+const info = colors.bold.blue;
 
-await new Command()
-  .name("reverse-proxy")
-  .description("A simple reverse proxy example cli.")
-  .version("v1.0.0")
-  .option("-p, --port <port:number>", "The port number for the local server.", {
-    default: 8080,
-  })
-  .option("--host <hostname>", "The host name for the local server.", {
-    default: "localhost",
-  })
-  .arguments("[domain]")
-  .action(async ({ port, host }, domain = "deno.land") => {
-    console.log(`Listening on http://${host}:${port}`);
-    await serve((req: Request) => {
-      const url = new URL(req.url);
-      url.protocol = "https:";
-      url.hostname = domain;
-      url.port = "443";
+console.log(info("This is an info message!"));
+console.log(warn("This is a warning!"));
+console.log(error("This is an error message!"));
+console.log(error.underline("This is a critical error message!"));
 
-      console.log("Proxy request to:", url.href);
-      return fetch(url.href, {
-        headers: req.headers,
-        method: req.method,
-        body: req.body,
-      });
-    }, { hostname: host, port });
-  })
-  .parse();
+await delay(3000);
+
+tty.cursorLeft.cursorUp(4).eraseDown();
