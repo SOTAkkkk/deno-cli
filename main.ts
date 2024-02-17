c12c1e19f8311df11da08eb03777492dd3b868d4
長岡聡太 <>
2024-02-17 13:58:57 +0900

    cli init

--- main.ts
+++ main.ts
@@ -1,7 +1,9 @@
 import { Command } from "https://deno.land/x/cliffy@v1.0.0-rc.3/command/mod.ts";
 
-await new Command()
-  .name("cliffy")
+new Command()
+  // Main command.
+  .name("gyomu")
   .version("0.1.0")
-  .description("Command line framework for Deno")
-  .parse(Deno.args);
+  .description("業務効率化ツール")
+  .usage("[options]")
+  .parse(Deno.args)
